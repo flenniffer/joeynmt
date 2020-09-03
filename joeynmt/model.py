cae2471e75861f47cfc555b89c3b588e6573f27f
flenniffer <flenniffer@gmail.com>
2020-09-03 16:28:07 +0200

    Added Unsupervised NMT model class

--- joeynmt/model.py
+++ joeynmt/model.py
@@ -197,6 +197,188 @@ class Model(nn.Module):
                    self.decoder, self.src_embed, self.trg_embed)
 
 
+class UnsupervisedNMTModel(nn.Module):
+    def __init__(self,
+                 src_embed: Embeddings,
+                 trg_embed: Embeddings,
+                 encoder: Encoder,
+                 src_decoder: Decoder,
+                 trg_decoder: Decoder,
+                 src_vocab: Vocabulary,
+                 trg_vocab: Vocabulary
+                 ) -> None:
+        """
+        Create a new unsupervised NMT model.
+        Architecture follows Artetxe et al. (2018): Unsupervised Neural Machine Translation.
+        TODO
+        :param src_embed:
+        :param trg_embed:
+        :param encoder:
+        :param src_decoder:
+        :param trg_decoder:
+        :param src_vocab:
+        :param trg_vocab:
+        """
+        super(UnsupervisedNMTModel, self).__init__()
+
+        self.src_embed = src_embed
+        self.trg_embed = trg_embed
+        self.shared_encoder = encoder
+        self.current_encoder_embeddings = None
+        self.src_decoder = src_decoder
+        self.trg_decoder = trg_decoder
+        self.current_decoder = None
+        self.src_vocab = src_vocab
+        self.trg_vocab = trg_vocab
+        self.src_specials = {BOS_TOKEN: self.src_vocab.stoi[BOS_TOKEN],
+                             PAD_TOKEN: self.src_vocab.stoi[PAD_TOKEN],
+                             EOS_TOKEN: self.src_vocab.stoi[EOS_TOKEN]}
+        self.trg_specials = {BOS_TOKEN: self.trg_vocab.stoi[BOS_TOKEN],
+                             PAD_TOKEN: self.trg_vocab.stoi[PAD_TOKEN],
+                             EOS_TOKEN: self.trg_vocab.stoi[EOS_TOKEN]}
+
+    def set_current_encoder_embeddings(self, encode_mode: str):
+        if encode_mode == "src":
+            self.current_encoder_embeddings = self.src_embed
+        elif encode_mode == "trg":
+            self.current_encoder_embeddings = self.trg_embed
+        else:
+            raise ValueError("Encoding mode must be either 'src' or 'trg'.")
+
+    def set_current_decoder(self, decode_mode: str):
+        if decode_mode == "src":
+            self.current_decoder = self.src_decoder
+        elif decode_mode == "trg":
+            self.current_decoder = self.trg_decoder
+        else:
+            raise ValueError("Decoding mode must be either 'src' or 'trg'.")
+
+    def forward(self, src_input: Tensor, trg_input: Tensor, src_mask: Tensor,
+                src_lengths: Tensor, trg_mask: Tensor = None) \
+            -> (Tensor, Tensor, Tensor, Tensor):
+        """
+        Encodes the input sentence using the current encoder embeddings.
+        Decodes the encoded input using the current decoder.
+        """
+        # Encode using current encoder embeddings
+        encoder_output, encoder_hidden = self.encode(src_input, src_lengths, src_mask)
+
+        unroll_steps = trg_input.size(1)
+        # Decode using current decoder
+        return self.decode(encoder_output,
+                           encoder_hidden,
+                           src_mask, trg_input,
+                           unroll_steps,
+                           trg_mask=trg_mask)
+
+    def encode(self, src_input: Tensor, src_length: Tensor, src_mask: Tensor) \
+            -> (Tensor, Tensor):
+        return self.shared_encoder(
+            self.current_encoder_embeddings(src_input), src_length, src_mask)
+
+    def decode(self, encoder_output: Tensor, encoder_hidden: Tensor,
+               src_mask: Tensor, trg_input: Tensor,
+               unroll_steps: int,
+               decoder_hidden: Tensor = None,
+               trg_mask: Tensor = None) \
+            -> (Tensor, Tensor, Tensor, Tensor):
+        return self.current_decoder(trg_embed=self.trg_embed(trg_input),
+                                    encoder_output=encoder_output,
+                                    encoder_hidden=encoder_hidden,
+                                    src_mask=src_mask,
+                                    unroll_steps=unroll_steps,
+                                    hidden=decoder_hidden,
+                                    trg_mask=trg_mask)
+
+    def get_loss_for_batch(self, batch: Batch, loss_function: nn.Module) \
+            -> Tensor:
+        """
+        Compute non-normalized loss and number of tokens for a batch
+
+        :param batch: batch to compute loss for
+        :param loss_function: loss function, computes for input and target
+            a scalar loss for the complete batch
+        :return: batch_loss: sum of losses over non-pad elements in the batch
+        """
+        # pylint: disable=unused-variable
+        out, hidden, att_probs, _ = self.forward(
+            src_input=batch.src, trg_input=batch.trg_input,
+            src_mask=batch.src_mask, src_lengths=batch.src_lengths,
+            trg_mask=batch.trg_mask)
+
+        # compute log probs
+        log_probs = F.log_softmax(out, dim=-1)
+
+        # compute batch loss
+        batch_loss = loss_function(log_probs, batch.trg)
+        # return batch loss = sum over all elements in batch that are not pad
+        return batch_loss
+
+    def run_batch(self, batch: Batch, max_output_length: int, beam_size: int,
+                  beam_alpha: float) -> (np.array, np.array):
+        """
+        Get outputs and attentions scores for a given batch
+
+        :param batch: batch to generate hypotheses for
+        :param max_output_length: maximum length of hypotheses
+        :param beam_size: size of the beam for beam search, if 0 use greedy
+        :param beam_alpha: alpha value for beam search
+        :return: stacked_output: hypotheses for batch,
+            stacked_attention_scores: attention scores for batch
+        """
+        # Encode using current encoder embeddings
+        encoder_output, encoder_hidden = self.encode(
+            batch.src, batch.src_lengths,
+            batch.src_mask)
+
+        # if maximum output length is not globally specified, adapt to src len
+        if max_output_length is None:
+            max_output_length = int(max(batch.src_lengths.cpu().numpy()) * 1.5)
+
+        if self.current_decoder == self.src_decoder:  # current target is source language
+            current_trg_embed = self.src_embed
+            current_trg_specials = self.src_specials
+        else:  # current target is target language
+            current_trg_embed = self.trg_embed
+            current_trg_specials = self.trg_specials
+
+        # greedy decoding
+        if beam_size < 2:
+            stacked_output, stacked_attention_scores = greedy(
+                    encoder_hidden=encoder_hidden,
+                    encoder_output=encoder_output, eos_index=current_trg_specials[EOS_TOKEN],
+                    src_mask=batch.src_mask, embed=current_trg_embed,
+                    bos_index=current_trg_specials[BOS_TOKEN], decoder=self.current_decoder,
+                    max_output_length=max_output_length)
+            # batch, time, max_src_length
+        else:  # beam size
+            stacked_output, stacked_attention_scores = \
+                    beam_search(
+                        size=beam_size, encoder_output=encoder_output,
+                        encoder_hidden=encoder_hidden,
+                        src_mask=batch.src_mask, embed=current_trg_embed,
+                        max_output_length=max_output_length,
+                        alpha=beam_alpha, eos_index=current_trg_specials[EOS_TOKEN],
+                        pad_index=current_trg_specials[PAD_TOKEN],
+                        bos_index=current_trg_specials[BOS_TOKEN],
+                        decoder=self.current_decoder)
+
+        return stacked_output, stacked_attention_scores
+
+    def __repr__(self) -> str:
+        """
+        String representation: a description of encoder, decoder and embeddings
+
+        :return: string representation
+        """
+        return f"{self.__class__.__name__}(\n" \
+               f"\tencoder={self.shared_encoder},\n" \
+               f"\tsrc_decoder={self.src_decoder},\n" \
+               f"\ttrg_decoder={self.trg_decoder},\n" \
+               f"\tsrc_embed={self.src_embed},\n" \
+               f"\ttrg_embed={self.trg_embed})"
+
+
 def build_model(cfg: dict = None,
                 src_vocab: Vocabulary = None,
                 trg_vocab: Vocabulary = None) -> Model:
@@ -208,6 +390,20 @@ def build_model(cfg: dict = None,
     :param trg_vocab: target vocabulary
     :return: built and initialized model
     """
+    if cfg.get("architecture", "encoder-decoder") == "encoder-decoder":
+        model = build_encoder_decoder_model(cfg, src_vocab, trg_vocab)
+    elif cfg.get("architecture", "encoder-decoder") == "unsupervised-nmt":
+        model = build_unsupervised_nmt_model(cfg, src_vocab, trg_vocab)
+    else:
+        raise ConfigurationError("Supported architectures: 'encoder-decoder' and 'unsupervised-nmt'")
+
+    return model
+
+
+def build_encoder_decoder_model(cfg: dict = None,
+                                src_vocab: Vocabulary = None,
+                                trg_vocab: Vocabulary = None) -> Model:
+
     src_padding_idx = src_vocab.stoi[PAD_TOKEN]
     trg_padding_idx = trg_vocab.stoi[PAD_TOKEN]
 
@@ -276,4 +472,64 @@ def build_model(cfg: dict = None,
     # custom initialization of model parameters
     initialize_model(model, cfg, src_padding_idx, trg_padding_idx)
 
+    return model
+
+
+def build_unsupervised_nmt_model(cfg: dict = None,
+                                 src_vocab: Vocabulary = None,
+                                 trg_vocab: Vocabulary = None) -> Model:
+    """
+    TODO
+    """
+    src_padding_idx = src_vocab.stoi[PAD_TOKEN]
+    trg_padding_idx = trg_vocab.stoi[PAD_TOKEN]
+
+    # build source and target embedding layers
+    src_embed = Embeddings(**cfg["encoder"]["embeddings"],
+                           vocab_size=len(src_vocab),
+                           padding_idx=src_padding_idx)
+    trg_embed = Embeddings(**cfg["decoder"]["embeddings"],
+                           vocab_size=len(trg_vocab),
+                           padding_idx=trg_padding_idx)
+
+    # build shared encoder
+    enc_dropout = cfg["encoder"].get("dropout", 0.)
+    enc_emb_dropout = cfg["encoder"]["embeddings"].get("dropout", enc_dropout)
+    if cfg["encoder"].get("type", "recurrent") == "transformer":
+        assert cfg["encoder"]["embeddings"]["embedding_dim"] == \
+               cfg["encoder"]["hidden_size"], \
+               "for transformer, emb_size must be hidden_size"
+
+        shared_encoder = TransformerEncoder(**cfg["encoder"],
+                                            emb_size=src_embed.embedding_dim,
+                                            emb_dropout=enc_emb_dropout)
+    else:
+        shared_encoder = RecurrentEncoder(**cfg["encoder"],
+                                          emb_size=src_embed.embedding_dim,
+                                          emb_dropout=enc_emb_dropout)
+
+    # build source and target decoder
+    dec_dropout = cfg["decoder"].get("dropout", 0.)
+    dec_emb_dropout = cfg["decoder"]["embeddings"].get("dropout", dec_dropout)
+    if cfg["decoder"].get("type", "recurrent") == "transformer":
+        src_decoder = TransformerDecoder(
+            **cfg["decoder"], encoder=shared_encoder, vocab_size=len(src_vocab),
+            emb_size=src_embed.embedding_dim, emb_dropout=dec_emb_dropout)
+        trg_decoder = TransformerDecoder(
+            **cfg["decoder"], encoder=shared_encoder, vocab_size=len(trg_vocab),
+            emb_size=trg_embed.embedding_dim, emb_dropout=dec_emb_dropout)
+    else:
+        src_decoder = RecurrentDecoder(
+            **cfg["decoder"], encoder=shared_encoder, vocab_size=len(src_vocab),
+            emb_size=src_embed.embedding_dim, emb_dropout=dec_emb_dropout)
+        trg_decoder = RecurrentDecoder(
+            **cfg["decoder"], encoder=shared_encoder, vocab_size=len(trg_vocab),
+            emb_size=trg_embed.embedding_dim, emb_dropout=dec_emb_dropout)
+
+    model = UnsupervisedNMTModel(src_embed, trg_embed,
+                                 shared_encoder,
+                                 src_decoder, trg_decoder,
+                                 src_vocab, trg_vocab)
+    # initialise model
+    # TODO: Initialisers shouldn't overwrite model
     return model
